aa9f10e89660d05adab5deb06ec009b6d3d3ae43
StDoguinho <144288491+StDoguinho@users.noreply.github.com>
2023-11-18 15:40:16 -0300

    .

--- javaScript/tempoImagemSemInicial.js
+++ javaScript/tempoImagemSemInicial.js
@@ -3,6 +3,13 @@ var texto = document.querySelector(".caixaTextoIntroduçao");
 var botaoVerNovamente = document.querySelector('.botaoVerNovamente');
 var subtituloIntroduçao = document.querySelector('.subtituloIntroduçao');
 
+backgroundImage.addEventListener("click", ()=>{
+    backgroundImage.style.display = 'none';
+    texto.style.display = 'flex';
+    botaoVerNovamente.style.display = 'flex';
+    subtituloIntroduçao.style.display = 'flex';
+})
+
 function removerImagem() {
     backgroundImage.style.display = 'none';
     texto.style.display = 'flex';
